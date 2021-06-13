ac8e5246388f94b323d0392e0330a0972d11d3df
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2021-06-13 08:59:17 -0400

    Splatter ParticleSystem

--- Assets/Scripts/Combat/SlimeSplatter.cs
+++ Assets/Scripts/Combat/SlimeSplatter.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 namespace Game.Combat {
     public class SlimeSplatter : GFXobject
     {
-
+        
 
         protected override void Start() {
             base.Start();
+        
+            ParticleSystem ps = GetComponentInChildren<ParticleSystem>();
+            var main = ps.main;
+            main.startColor = baseColor;
+            ps.transform.localScale *= transform.localScale.x;
+
             // TODO: Play Splatter SFX !!!    
         }
 
