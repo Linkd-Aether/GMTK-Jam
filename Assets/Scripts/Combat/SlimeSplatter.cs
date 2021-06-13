bc1e7d67ce5f333542ef743f194534f17d788373
Jack McEvoy <jackmcevoy15@gmail.com>
2021-06-13 13:09:33 -0400

    Slime Splatter and Absorb Sounds

--- Assets/Scripts/Combat/SlimeSplatter.cs
+++ Assets/Scripts/Combat/SlimeSplatter.cs
@@ -6,17 +6,22 @@ using UnityEngine;
 namespace Game.Combat {
     public class SlimeSplatter : GFXobject
     {
-        
+
+        public AudioClip[] clips;
+        private AudioSource source;
+
 
         protected override void Start() {
             base.Start();
         
-            ParticleSystem ps = GetComponentInChildren<ParticleSystem>();
-            var main = ps.main;
-            main.startColor = baseColor;
-            ps.transform.localScale *= transform.localScale.x;
+            // ParticleSystem ps = GetComponentInChildren<ParticleSystem>();
+            // var main = ps.main;
+            // main.startColor = baseColor;
+            // ps.transform.localScale *= transform.localScale.x;
 
-            // TODO: Play Splatter SFX !!!    
+            source = GetComponent<AudioSource>();
+            source.clip = clips[Random.Range(0, clips.Length)];
+            source.Play();
         }
 
         protected virtual void EndSplatter() {
